c4cb6c4398cb0bc3e85f90ebdcc37d5871bb86c0
Karimatif33 <karimatif33@gmail.com>
2024-01-09 07:44:06 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Product from "./pages/Product";
+import Pricing from "./pages/Pricing";
+import Homepage from "./pages/Homepage";
+import AppLayout from "./pages/AppLayout";
+import PageNotFound from "./pages/PageNotFound";
+import Login from "./pages/Login";
+import CityList from "./components/CityList";
+import CountryList from "./components/CountryList";
+import City from "./components/City";
+import Form from "./components/Form";
+import { CitiesProvider } from "./context/citiesContext";
+
+function App() {
+  return (
+    <CitiesProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Homepage />} />
+          <Route path="pricing" element={<Pricing />} />
+          <Route path="product" element={<Product />} />
+          <Route path="login" element={<Login />} />
+          <Route path="app" element={<AppLayout />}>
+            <Route path="cities" element={<CityList />} />
+            <Route path="cities/:id" element={<City />}></Route>
+            <Route path="countries" element={<CountryList />} />
+            <Route path="form" element={<Form />} />
+          </Route>
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </CitiesProvider>
+  );
+}
+
+export default App;
---
+++ src/components/Map.jsx
@@ -0,0 +1,38 @@
+import { useNavigate, useSearchParams } from 'react-router-dom'
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+import styles from './Map.module.css'
+import { useState } from 'react'
+import { useCities } from '../context/citiesContext'
+
+function Map() {
+   const navigate = useNavigate()
+   const {cities} = useCities()
+   const [mapPosition, setMapPosition] = useState([40, 0])
+    // const [searchParams, setSearchParams] = useSearchParams()
+    // const lat = searchParams.get("lat")
+    // const lng = searchParams.get("lng") 
+    return (
+        <div className={styles.mapContainer} onClick={()=>{navigate("form")}}>
+            
+            
+
+<MapContainer center={mapPosition} zoom={13} scrollWheelZoom={true} className={styles.map}>
+    <TileLayer
+      attribution='&copy; <a href="https://www.openstreetmap.fr/hot/copyright">OpenStreetMap</a> contributors'
+      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+    />
+    {cities.map(city => (<Marker position={[city.position.lat, city.position.lng]} key={city.id}>
+      <Popup>
+        <span>{city.emoji}</span>
+        <span>{city.cityName}</span>
+
+      </Popup>
+    </Marker>))}
+  </MapContainer>
+
+
+        </div>
+    )
+}
+
+export default Map
